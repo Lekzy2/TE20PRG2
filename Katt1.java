734c1718ad02c4a374c2bc0e92759c9d92e3a268
Lekzy2 <77719484+Lekzy2@users.noreply.github.com>
2022-08-25 13:38:58 +0200

    eeeeh idk

---
+++ Katt1.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
+
+public class Katt1 {
+    public static void main(String[] args) {
+
+        String greeting = JOptionPane.showInputDialog("k");
+        for(int i = 0; i + 1 <= message.lenght(); i++){
+        char c = message.CharAt(i);
+        
+        }
+    }
+}
