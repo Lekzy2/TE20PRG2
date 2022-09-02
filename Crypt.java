d376dbb1cd5b0882304efacb1f5286f1c60ac1b8
Lekzy2 <77719484+Lekzy2@users.noreply.github.com>
2022-09-02 13:32:58 +0200

    idk

--- Crypt.java
+++ Crypt.java
@@ -1,5 +1,27 @@
+import javax.swing.JOptionPane;
+
 public class Crypt {
   public static void main(String[] args) {
-      
+    // m = message k = key
+    String m = JOptionPane.showInputDialog("sauce");
+    String krypt = "";
+    char k = ')';
+    int i = Integer.parseInt(m);
+    String n = Integer.toBinaryString(i);
+    int b = Integer.parseInt(n);
+    String tobinary = JOptionPane.showMessageDialog();
   }  
 }
+
+
+
+//*
+//*Static int crypt(int m,int k){
+//*  int c = m ^ k;
+//*  return c;
+//*}
+//*String message = JOptionPane.showInputDialog("give us your semen sauece");
+    
+
+//*int i;
+//*JOptionPane.showMessageDialog(Integer.toBinaryString(i)); *
